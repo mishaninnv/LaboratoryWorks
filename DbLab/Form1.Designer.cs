fab33899fd67725dbd5e967f46cb40678136d793
mishaninnv <mishaninnv93@gmail.com>
2021-06-07 06:40:29 +0500

    Make project DbLab

--- DbLab/Form1.Designer.cs
+++ DbLab/Form1.Designer.cs
@@ -29,20 +29,361 @@ namespace DbLab
         /// </summary>
         private void InitializeComponent()
         {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column11 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.Column10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column14 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView4 = new System.Windows.Forms.DataGridView();
+            this.Column12 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column13 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView5 = new System.Windows.Forms.DataGridView();
+            this.Column15 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column16 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column17 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView6 = new System.Windows.Forms.DataGridView();
+            this.Column18 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column19 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView6)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7,
+            this.Column8,
+            this.Column9});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 49);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(854, 341);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Фамилия";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Номер";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Пол";
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Город";
+            this.Column5.Name = "Column5";
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Дата заезда";
+            this.Column6.Name = "Column6";
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "Дата отъезда";
+            this.Column7.Name = "Column7";
+            // 
+            // Column8
+            // 
+            this.Column8.HeaderText = "Плата за проживание";
+            this.Column8.Name = "Column8";
+            // 
+            // Column9
+            // 
+            this.Column9.HeaderText = "Плата за услуги";
+            this.Column9.Name = "Column9";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column11});
+            this.dataGridView2.Location = new System.Drawing.Point(1084, 49);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 25;
+            this.dataGridView2.Size = new System.Drawing.Size(359, 341);
+            this.dataGridView2.TabIndex = 2;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Фамилия";
+            this.Column1.Name = "Column1";
+            // 
+            // Column11
+            // 
+            this.Column11.HeaderText = "Плата за услуги";
+            this.Column11.Name = "Column11";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 864);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(286, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Количество клиентов, проживавших в 505 номере";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 893);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(227, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Суммарная стоимость оказанных услуг";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Таблица Данные";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1084, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(359, 15);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Постояльцы которым, не оказывались дополнительные услуги";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 429);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(240, 15);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Постояльцы, заехавшие в марте 2012 года";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(321, 429);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(139, 15);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Постояльцы из Москвы";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(641, 429);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(127, 15);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Мужчины их Тюмени";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(1043, 429);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(301, 15);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "Итоговая сумма за проживание для каждого клиента";
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column10,
+            this.Column14});
+            this.dataGridView3.Location = new System.Drawing.Point(13, 463);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.RowTemplate.Height = 25;
+            this.dataGridView3.Size = new System.Drawing.Size(257, 341);
+            this.dataGridView3.TabIndex = 11;
+            // 
+            // Column10
+            // 
+            this.Column10.HeaderText = "Фамилия";
+            this.Column10.Name = "Column10";
+            // 
+            // Column14
+            // 
+            this.Column14.HeaderText = "Дата заезда";
+            this.Column14.Name = "Column14";
+            // 
+            // dataGridView4
+            // 
+            this.dataGridView4.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView4.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column12,
+            this.Column13});
+            this.dataGridView4.Location = new System.Drawing.Point(321, 463);
+            this.dataGridView4.Name = "dataGridView4";
+            this.dataGridView4.RowTemplate.Height = 25;
+            this.dataGridView4.Size = new System.Drawing.Size(262, 341);
+            this.dataGridView4.TabIndex = 12;
+            // 
+            // Column12
+            // 
+            this.Column12.HeaderText = "Фамилия";
+            this.Column12.Name = "Column12";
+            // 
+            // Column13
+            // 
+            this.Column13.HeaderText = "Город";
+            this.Column13.Name = "Column13";
+            // 
+            // dataGridView5
+            // 
+            this.dataGridView5.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView5.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column15,
+            this.Column16,
+            this.Column17});
+            this.dataGridView5.Location = new System.Drawing.Point(641, 463);
+            this.dataGridView5.Name = "dataGridView5";
+            this.dataGridView5.RowTemplate.Height = 25;
+            this.dataGridView5.Size = new System.Drawing.Size(344, 341);
+            this.dataGridView5.TabIndex = 13;
+            // 
+            // Column15
+            // 
+            this.Column15.HeaderText = "Фамилия";
+            this.Column15.Name = "Column15";
+            // 
+            // Column16
+            // 
+            this.Column16.HeaderText = "Пол";
+            this.Column16.Name = "Column16";
+            // 
+            // Column17
+            // 
+            this.Column17.HeaderText = "Город";
+            this.Column17.Name = "Column17";
+            // 
+            // dataGridView6
+            // 
+            this.dataGridView6.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView6.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column18,
+            this.Column19});
+            this.dataGridView6.Location = new System.Drawing.Point(1043, 463);
+            this.dataGridView6.Name = "dataGridView6";
+            this.dataGridView6.RowTemplate.Height = 25;
+            this.dataGridView6.Size = new System.Drawing.Size(359, 341);
+            this.dataGridView6.TabIndex = 14;
+            // 
+            // Column18
+            // 
+            this.Column18.HeaderText = "Фамилия";
+            this.Column18.Name = "Column18";
+            // 
+            // Column19
+            // 
+            this.Column19.HeaderText = "Сумма за проживание";
+            this.Column19.Name = "Column19";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 634);
+            this.ClientSize = new System.Drawing.Size(1392, 932);
+            this.Controls.Add(this.dataGridView6);
+            this.Controls.Add(this.dataGridView5);
+            this.Controls.Add(this.dataGridView4);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView6)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column10;
+        private System.Windows.Forms.DataGridView dataGridView4;
+        private System.Windows.Forms.DataGridView dataGridView5;
+        private System.Windows.Forms.DataGridView dataGridView6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column11;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column12;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column13;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column14;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column15;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column16;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column17;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column18;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column19;
     }
 }
 
--- DbLab/Form1.cs
+++ DbLab/Form1.cs
@@ -13,13 +13,119 @@ namespace DbLab
 {
     public partial class Form1 : Form
     {
-        
+        string connectionString = @"Provider=Microsoft.Jet.OLEDB.4.0; Data Source=|DataDirectory|\Постояльцы.mdb";
+        private OleDbConnection connection;
+
         public Form1()
         {
             InitializeComponent();
-            string connectionString = @"Data Source=|DataDirectory|\Постояльцы.mdb";
-            
+
+            connection = new OleDbConnection(connectionString);
+            connection.Open();
+
+            var query = "SELECT * FROM Данные";
+            OleDbCommand cmd = new OleDbCommand(query, connection);
+
+            OleDbDataReader reader = cmd.ExecuteReader();
+
+            var buffer = new List<string>();
+            while (reader.Read())
+            {
+                for (var i = 0; i < reader.FieldCount; i++)
+                {
+                    buffer.Add(reader[i].ToString());
+                }
+                dataGridView1.Rows.Add(buffer.ToArray());
+                buffer.Clear();
+            }
+
+            query = "SELECT Фамилия, Плата_за_услуги FROM Данные Where Плата_за_услуги = 0";
+            cmd = new OleDbCommand(query, connection);
+            reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                dataGridView2.Rows.Add(new string[] { reader[0].ToString(), reader[1].ToString() });
+            }
+
+
+            query = "SELECT Фамилия, Дата_заезда FROM Данные";
+            cmd = new OleDbCommand(query, connection);
+            reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                var currDate = Convert.ToDateTime(reader[1]);
+                if (currDate.Month == 03)
+                {
+                    dataGridView3.Rows.Add(new string[] { reader[0].ToString(), reader[1].ToString() });
+                }
+            }
+
+
+            query = "SELECT Фамилия, Город FROM Данные Where Город = 'Москва'";
+            cmd = new OleDbCommand(query, connection);
+            reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                dataGridView4.Rows.Add(new string[] { reader[0].ToString(), reader[1].ToString() });
+            }
+
+
+            query = "SELECT Фамилия, Город FROM Данные Where Город = 'Москва'";
+            cmd = new OleDbCommand(query, connection);
+            reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                dataGridView4.Rows.Add(new string[] { reader[0].ToString(), reader[1].ToString() });
+            }
+
+
+            query = "SELECT Фамилия, Пол, Город FROM Данные Where Пол = 'муж'";
+            cmd = new OleDbCommand(query, connection);
+            reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                if (reader[2].Equals("Тюмень"))
+                {
+                    dataGridView5.Rows.Add(new string[] { reader[0].ToString(), reader[1].ToString(), reader[2].ToString() });
+                }
+            }
+
+
+            query = "SELECT * FROM Данные";
+            cmd = new OleDbCommand(query, connection);
+            reader = cmd.ExecuteReader();
+            var allPrice = 0;
+            while (reader.Read())
+            {
+                var priceOne = Convert.ToInt32(reader[6]);
+                var priceTwo = Convert.ToInt32(reader[7]);
+                var sum = priceOne + priceTwo;
+
+                allPrice += sum;
+
+                dataGridView6.Rows.Add(new string[] { reader[0].ToString(), sum.ToString() });
+            }
+            label2.Text = $"Суммарная стоимость оказанных услуг: {allPrice}";
+
+
+            query = "SELECT Номер FROM Данные WHERE Номер = 505";
+            cmd = new OleDbCommand(query, connection);
+            reader = cmd.ExecuteReader();
+            var counter = 0;
+            while (reader.Read())
+            {
+                counter++;
+            }
+            label1.Text = $"Количество клиентов, проживавших в 505 номере: {counter}";
+
+
+
+            reader.Close();
         }
 
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            connection.Close();
+        }
     }
 }
